09d297ee0ab4099a202e97b1a079765d7c437459
kantarogit <kantarofilip@gmail.com>
2021-08-17 00:17:17 +0200

    chrome driver details
    Signed-off-by: kantarogit <kantarofilip@gmail.com>

--- src/main/java/utils/WebDriverFactory.java
+++ src/main/java/utils/WebDriverFactory.java
@@ -9,6 +9,8 @@ public class WebDriverFactory {
     public static WebDriver getDriver() {
 
         if (driver == null) {
+            // set this according to the local setup or copy chromedriver version as per Chrome browser version
+            // https://chromedriver.chromium.org/downloads
             System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
             driver = new ChromeDriver();
         }
